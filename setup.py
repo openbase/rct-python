dd80f46e509227456944f5d68d4b156544461a05
Divine Threepwood <divine@openbase.org>
2019-09-19 17:42:04 +0200

    removed python name out of project id

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ version_file = os.path.realpath(__file__)
 version_file = os.path.join(version_file[:version_file.rfind("/")], "VERSION")
 version = open(version_file).read().strip()
 
-setup(name="openbase-rct-python",
+setup(name="openbase-rct",
       version=version,
       description="Robotics Coordinate Transform (python).",
       long_description="This library wraps the functionality of the tf2 library (originated from ROS) and supports communication over the RSB middleware. The ROS dependencies are minimised as good as possible.",
       author="Norman Koester",
       author_email="nkoester[at]techfak.uni-bielefeld.de",
-      url="TODO",
+      url="https://github.com/openbase/rct-python.git",
       download_url="TODO",
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       include_package_data=True,
@@ -29,4 +29,5 @@ setup(name="openbase-rct-python",
           'Topic :: Text Processing :: Markup :: XML'
       ],
       # 'Louie', 'suds', 'restlib',
-      install_requires=['pyrr', "rsb-python>=0.18", "openbase-type"])
+      # non-standard requirements: openbase-rct, rsb-python
+      install_requires=['pyrr', "openbase-type"])
